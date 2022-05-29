ad42e667ac5e2d2e52e106eaacd6932f48e4fdb7
janisandzans <janis_andzans@inbox.lv>
2022-05-29 13:52:48 +0300

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+        /**
+         * The attributes that are mass assignable.
+         * 
+         * @var array<int, string>
+         */
+    protected $fillable = [
+        'title',
+        'body',
+        'author_name'
+    ];
+
+    protected static function newFactory(): PostFactory
+    {
+        return new PostFactory();
+    }
+
+    public function comments(): MorphMany
+    {
+    return $this->morphMany(Comment::class, 'commentable');
+    }
+}
